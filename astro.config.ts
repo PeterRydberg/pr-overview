91c1d2f462d214db7a25388e25a7227d11b977e7
Peter Rydberg <hmp.rydberg@gmail.com>
2026-02-25 12:58:01 +0100

    Add some basic Github authentication

--- astro.config.ts
+++ astro.config.ts
@@ -1,9 +1,20 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig, envField } from "astro/config";
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  integrations: [react()],
+  env: {
+    schema: {
+      APP_NAME: envField.string({ context: "client", access: "public" }),
+
+      BETTER_AUTH_URL: envField.string({ context: "server", access: "public" }),
+      BETTER_AUTH_SECRET: envField.string({ context: "server", access: "secret" }),
+
+      GITHUB_CLIENT_ID: envField.string({ context: "server", access: "public" }),
+      GITHUB_CLIENT_SECRET: envField.string({ context: "server", access: "secret" }),
+    },
+  },
 });
