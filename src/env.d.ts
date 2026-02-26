7d4f8aac9fff96d883ae887f9e8646743c5f0124
Peter Rydberg <hmp.rydberg@gmail.com>
2026-02-26 09:03:27 +0100

    Fetch repo information from Github

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,8 +1,16 @@
 /// <reference path="../.astro/types.d.ts" />
 
+type AccessToken = {
+  accessToken: string;
+  accessTokenExpiresAt: Date | undefined;
+  scopes: string[];
+  idToken: string | undefined;
+};
+
 declare namespace App {
   interface Locals {
     user: import("better-auth").User | null;
     session: import("better-auth").Session | null;
+    accessToken: AccessToken | null;
   }
 }
